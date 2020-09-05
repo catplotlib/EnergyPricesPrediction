3a9c9917ce972f8e0101128bbd26e8b05fffec45
Puja Chaudhury <61319491+catplotlib@users.noreply.github.com>
2020-09-05 16:17:25 +0530

    Update SimpleLinearRegression.py

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -1,7 +1,6 @@
 #import packages
 import pandas as pd
 pd.options.mode.chained_assignment = None
-
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
@@ -17,15 +16,11 @@ plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams["font.size"] = 18
 
 #read the file
-df = pd.read_csv('8daysMin - PriceMinute.csv')
-
-#sorting
-data = df.sort_index(ascending=True, axis=0)
+data = pd.read_csv('8daysMin - PriceMinute.csv')
 
 #splitting training and validation Data
 train = data[:518]
 valid = data[518:]
-
 
 #selecting relevant columns
 x_train = train.drop('MCP', axis=1)
@@ -48,7 +43,6 @@ x=valid['MCP'].values
 x = x.reshape(-1,1)
 error=r2_score(x, preds, sample_weight=None, multioutput='uniform_average')
 
-
 #setting indexes for graphs
 valid['Predictions'] = preds
 valid.index = data[518:].index
@@ -56,16 +50,16 @@ train.index = data[:518].index
 
 #plotting the training data and new Predictions
 xmin, xmax = plt.xlim()
-ymin, ymax = plt.ylim()
 plt.plot(train['MCP'])
 plt.plot(valid[['MCP', 'Predictions']])
 plt.xlabel('Days (Data from 11/07/2020 to 18/07/2020)')
 plt.ylabel('MCP (Market Clearing Price) in Rupees/MWh')
 plt.xlim([0,xmax])
 plt.title('Weekly Prediction')
-plt.xticks([0,100,200,300,400,500,600,700],['Saturday','Sunday','Monday',
+plt.xticks([0,96,192,288,384,480,576,672],['Saturday','Sunday','Monday',
                                             'Tuesday','Wednesday', 'Thursday',
                                             'Friday','Saturday'])
+
 blue_patch = mpatches.Patch(color='#5497c5', label='Training Data(Price)')
 orange_patch = mpatches.Patch(color='#ff902e', label='Validating Data(Price)')
 green_patch = mpatches.Patch(color='#3ba73b', label='Prediction(Price)')
