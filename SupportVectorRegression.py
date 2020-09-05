82ed07a8e7602d39abfa54569e021c231c1aa1fa
Puja Chaudhury <61319491+catplotlib@users.noreply.github.com>
2020-09-05 17:33:34 +0530

    Update SupportVectorRegression.py

--- SupportVectorRegression.py
+++ SupportVectorRegression.py
@@ -1,7 +1,9 @@
 #import packages
 import pandas as pd
+pd.options.mode.chained_assignment = None
 import numpy as np
 from sklearn.svm import SVR
+from sklearn.metrics import r2_score
 
 #to plot within notebook
 import matplotlib.pyplot as plt
@@ -14,21 +16,13 @@ plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams["font.size"] = 18
 
 #read the file
-df = pd.read_csv('8daysMin - PriceMinute.csv')
-
-#for normalizing data
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler(feature_range=(0, 1))
-
-#setting index as date
-df.index = df['Date']
-
-#sorting
-data = df.sort_index(ascending=True, axis=0)
+data = pd.read_csv('8daysMin - PriceMinute.csv')
 
 #splitting training and validation Data
+
 train = data[:518]
 valid = data[518:]
+
 
 #selecting relevant columns
 x_train = train.drop('MCP', axis=1)
@@ -37,20 +31,19 @@ x_valid = valid.drop('MCP', axis=1)
 y_valid = valid['MCP']
 
 #Selecting and training the machine learning model
-svr_rbf = SVR(kernel= 'rbf', C= 1e3, gamma= 0.1)
-svr_rbf.fit(x_train,y_train)
+model = SVR(kernel= 'rbf', C= 1e3, gamma= 'scale')
+model.fit(x_train,y_train)
 
 #Predicting the values
-preds = svr_rbf.predict(x_valid)
+preds = model.predict(x_valid)
 
 #root means square error values
 rms=np.sqrt(np.mean(np.power((np.array(y_valid)-np.array(preds)),2)))
 
 #r2 scores
 x=valid['MCP'].values
-x = x.reshape(-1,1)
-from sklearn.metrics import r2_score
 error=r2_score(x, preds, sample_weight=None, multioutput='uniform_average')
+
 
 #setting indexes for graphs
 valid['Predictions'] = preds
@@ -59,21 +52,18 @@ train.index = data[:518].index
 
 #plotting the training data and new Predictions
 xmin, xmax = plt.xlim()
-ymin, ymax = plt.ylim()
-plt.figure(figsize = (20,10))
 plt.plot(train['MCP'])
 plt.plot(valid[['MCP', 'Predictions']])
-plt.xlabel('Days (Data 11/07/2020 to 18/07/2020)')
+plt.xlabel('Days (Data from 11/07/2020 to 18/07/2020)')
 plt.ylabel('MCP (Market Clearing Price) in Rupees/MWh')
 plt.xlim([0,xmax])
 plt.title('Weekly Prediction')
-plt.xticks([0,100,200,300,400,500,600,700],['Saturday','Sunday','Monday',
+plt.xticks([0,96,192,288,384,480,576,672],['Saturday','Sunday','Monday',
                                             'Tuesday','Wednesday', 'Thursday',
                                             'Friday','Saturday'])
+
 blue_patch = mpatches.Patch(color='#5497c5', label='Training Data(Price)')
 orange_patch = mpatches.Patch(color='#ff902e', label='Validating Data(Price)')
 green_patch = mpatches.Patch(color='#3ba73b', label='Prediction(Price)')
-plt.legend(handles=[blue_patch,orange_patch,green_patch])                                        
-plt.show() 
-
-
+plt.legend(handles=[blue_patch,orange_patch,green_patch])                                         
+plt.show()
